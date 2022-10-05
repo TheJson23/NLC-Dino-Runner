c20632baed81607cd874e296a751342f3c6ab0e8
TheJson23 <bryanjeisson64@gmail.com>
2022-10-05 11:03:37 -0400

    Update dinosaur.py

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,14 +1,14 @@
-from typing_extensions import Self
 import pygame
 
-from sre_constants import JUMP
 from dino_runner.utils.constants import RUNNING
 from pygame.sprite import Sprite
+from dino_runner.utils.constants import JUMPING
 
 class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_VEL = 8.5
+    
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -18,11 +18,13 @@ class Dinosaur(Sprite):
         self.dino_run = True
         self.dino_jum = False
         self.jump_vel = self.JUMP_VEL
+    
     def update(self,user_imput):
         if self.dino_run:
             self.run()
         elif self.dino_jum:
             self.jump()
+        
         
         if user_imput[pygame.K_UP] and self.dino_jum:
             self.dino_jum = True
@@ -36,7 +38,14 @@ class Dinosaur(Sprite):
 
    
     def jump(self):
-        pass
+        self.image = JUMPING
+        if self.dino_jum:
+            self.dino_rect.y -= self.jump * 4
+            self.jump_vel -= 8
+        if self.jump_vel < -self.JUMP_VEL:
+            self.dino_rect.y = self.Y_POS
+            self.jum_vel = self.JUMP_VEL
+        
     def run(self):
         self.image = RUNNING[0] if  self.step_index < 5 else RUNNING[1]
         self.dino_rect = self.image.get_rect()
@@ -44,5 +53,5 @@ class Dinosaur(Sprite):
         self.dino_rect.y = self.Y_POS
         self.step_index += 1
 
-    def draw(self,screen:pygame.surface):
-        screen.blit(self.image,(self.dino_rect.x,self.dino_rect.y))
+    def draw(self,screen:pygame.Surface):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
