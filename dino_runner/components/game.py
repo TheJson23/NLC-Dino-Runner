91259379dfb1e2d58abfb4a5d2877c7a034ddd53
TheJson23 <bryanjeisson64@gmail.com>
2022-10-07 14:51:51 -0400

    clase 4

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,7 @@ import pygame
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManger
 from dino_runner.utils.constants     import BG, ICON, RUNNING, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 FONT_STYLE = "freesansbold.ttf"
 
 class Game:
@@ -9,17 +10,18 @@ class Game:
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.player = Dinosaur()
+        self.screen           = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock            = pygame.time.Clock()
+        self.player           = Dinosaur()
         self.obstacle_manager = ObstacleManger()
-        self.playing = False
-        self.game_speed = 20
-        self.x_pos_bg = 0
-        self.runnig = False
-        self.y_pos_bg = 380
-        self.point = 0
-        self.death_count = 0
+        self.power_up_manager = PowerUpManager()
+        self.playing          = False
+        self.game_speed       = 20
+        self.x_pos_bg         = 0
+        self.runnig           = False
+        self.y_pos_bg         = 380
+        self.point            = 0
+        self.death_count      = 0
         
     
     def execute(self):
@@ -34,7 +36,10 @@ class Game:
     def run(self):
         #Game loop: events - update - draw
         self.obstacle_manager.reset_obstacles()
-        self.playing = True
+        self.power_up_manager.reset_power_ups()
+        self.playing    = True
+        self.game_speed = 20
+        self.point      = 0
 
         while self.playing:
             self.events()
@@ -51,12 +56,14 @@ class Game:
         self.update_score()
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        
         self.obstacle_manager.update(self)
+        self.power_up_manager.update(self.point, self.game_speed,self.player)
         #
     def update_score(self):
         self.point += 1
         if self.point % 100 == 0:
-            self.game_speed += 5
+            self.game_speed += 1
 
     def draw_score(self):
         font = pygame.font.Font(FONT_STYLE,30)
@@ -72,6 +79,7 @@ class Game:
         self.draw_score()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
