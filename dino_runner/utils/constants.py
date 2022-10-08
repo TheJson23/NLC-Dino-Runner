7decf9df6a42a4159571b3b851343f3de9d72435
TheJson23 <bryanjeisson64@gmail.com>
2022-10-08 03:31:46 -0400

    Clase 5 tarea 5
    Me rindo
    aver si agrego algo mas
    fgjkjhkghfgjsjklfgsgjklññjlgkjklsfdgjkdfgklfdsgjkfgd´jksfdñjkgfñkfdgjkfjkjhsgdjhlgsñrgh

--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -19,7 +19,7 @@ RUNNING = [
 
 RUNNING_SHIELD = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1Shield.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2Shield.png")),
 ]
 
 RUNNING_HAMMER = [
@@ -38,12 +38,12 @@ DUCKING = [
 
 DUCKING_SHIELD = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Shield.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2Shield.png")),
 ]
 
 DUCKING_HAMMER = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2Hammer.png")),
 ]
 
 SMALL_CACTUS = [
